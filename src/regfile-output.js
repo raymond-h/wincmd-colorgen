f43c52bd32f7c430c18b13ffec42b541e85b5a21
Raymond Hammarling <raykay.rh@gmail.com>
2015-08-03 02:41:02 +0200

    Ensure there are quotes around keys

--- src/regfile-output.js
+++ src/regfile-output.js
@@ -1,7 +1,17 @@
-import ini from 'ini';
+export function toIniLike(obj) {
+    let str = '';
+    for(const i in obj) {
+        str += '[' + i + ']\n';
+
+        for(const j in obj[i]) {
+            str += `"${j}"=${obj[i][j]}` + '\n';
+        }
+    }
+    return str;
+}
 
 export function stringify(obj) {
     return `Windows Registry Editor Version 5.00
 
-${ ini.stringify(obj) }`;
+${ toIniLike(obj) }`;
 }
